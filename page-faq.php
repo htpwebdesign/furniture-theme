fbf0f6fd0b093fc5da6acd172664021e967ebe25
MGravitee <mgravitee@gmail.com>
2024-10-12 15:09:28 -0700

    quickly changed the template info at the top of front-page and have the content displaying properly on page-faq

--- page-faq.php
+++ page-faq.php
@@ -14,19 +14,30 @@ get_header();
 <main id="primary" class="site-main">
 
 	<?php
-	while (have_posts()) :
-		the_post();
+	while (have_posts()) : the_post();
+
+		//since ACF 6.25 wp_kses is used on get_field/sub_field so no escaping needed.
+
+		// Check rows exists.
+		if (have_rows('faq')):
+
+			// Loop through rows.
+			while (have_rows('faq')) : the_row();
+
+				if (function_exists('get_sub_field')) {
+					if (get_sub_field('heading')) { ?>
+						<h3><?php the_sub_field('heading'); ?></h3> <?php }
+																if (get_sub_field('content')) { ?>
+						<p><?php the_sub_field('content'); ?></p> <?php }
+															}
 
 
-
-
-
-
-
-
-
-	endwhile; // End of the loop.
-	?>
+														// End loop.
+														endwhile;
+													//end if
+													endif;
+												endwhile; // End of the loop.
+																	?>
 
 </main><!-- #main -->
 
