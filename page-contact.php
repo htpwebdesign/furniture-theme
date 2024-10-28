69bcefc115ca3380931a6acade13ccf5a0ddd76d
MGravitee <mgravitee@gmail.com>
2024-10-28 16:03:52 -0700

    Added styling to contact page, also figured out how to incorporate googlemap functionality

--- page-contact.php
+++ page-contact.php
@@ -16,33 +16,38 @@ get_header();
     <?php while (have_posts()) : the_post(); ?>
 
         <?php if (function_exists('the_field')): ?>
-            <header class="hero-banner contact-banner half-banner">
-                <div class="contact-banner-text banner-content">
-                    <h1><?php the_title(); ?></h1>
+            <header class="hero-banner contact-banner">
+                <div id="contact-banner-text" class="contact-banner-text banner-content">
+                    <h1 class="contact-title"><?php the_title(); ?></h1>
                 </div>
                 <?php if (get_field('banner_image')):
                     $banner_image = get_field('banner_image');
                     echo wp_get_attachment_image($banner_image, 'full'); ?>
                 <?php endif; ?>
-            </header>
-            <section class="contact-info">
-                <div class="contact-details">
-                    <address>
-                        <?php if (get_field('physical_address', 'option')): ?>
-                            <p><?php the_field('physical_address', 'option'); ?></p>
+                <section class="contact-info">
+                    <div class="contact-details">
+                        <address>
+                            <?php if (get_field('physical_address', 'option')): ?>
+                                <p><?php the_field('physical_address', 'option'); ?></p>
+                            <?php endif; ?>
+                        </address>
+                        <?php if (get_field('phone_number', 'option')): ?>
+                            <p>Phone: <a href=tel:+<?php the_field('phone_number', 'option'); ?>><?php the_field('phone_number', 'option'); ?></a></p>
                         <?php endif; ?>
-                    </address>
-                    <?php if (get_field('phone_number', 'option')): ?>
-                        <p>Phone: <a href=tel:+<?php the_field('phone_number', 'option'); ?>><?php the_field('phone_number', 'option'); ?></a></p>
-                    <?php endif; ?>
-
-                    <?php if (get_field('email', 'option')): ?>
-                        <p>Email: <a href="mailto:<?php the_field('email', 'option'); ?>"><?php the_field('email', 'option'); ?></a></p>
-                    <?php endif; ?>
-                </div>
-            </section>
+    
+                        <?php if (get_field('email', 'option')): ?>
+                            <p>Email: <a href="mailto:<?php the_field('email', 'option'); ?>"><?php the_field('email', 'option'); ?></a></p>
+                        <?php endif; ?>
+                    </div>
+                    <!-- googlemaps shortcode -->
+                    <div id="google-map" class="google-map" >
+                        <?php echo do_shortcode('[wpgmza id="1"]'); ?>
+                    </div>
+                </section>
+            </header>
         <?php endif; ?>
 
+        <!-- gravity forms shortcode -->
         <section class="contact-form">
             <?php echo do_shortcode('[gravityform id="1" title="true"]'); ?>
         </section>
