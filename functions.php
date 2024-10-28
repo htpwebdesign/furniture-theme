49326d22d9325081af05ac3389899455020353a7
Luke Rushton <luke.rushtonx@gmail.com>
2024-10-28 13:25:07 -0700

    styled the single product page

--- functions.php
+++ functions.php
@@ -207,3 +207,19 @@ function fwd_post_filter( $use_block_editor, $post ) {
     }
 }
 add_filter( 'use_block_editor_for_post', 'fwd_post_filter', 10, 2 );
+
+/**
+ *single product page hooks 
+ */
+//unsetting tabs
+function remove_product_tabs( $tabs ) {
+    unset( $tabs['description'] );
+    unset( $tabs['reviews'] );
+    unset( $tabs['additional_information'] );
+    return $tabs;
+}
+add_filter( 'woocommerce_product_tabs', 'remove_product_tabs', 98, 1 );
+
+add_action( 'woocommerce_before_single_product_summary', 'woocommerce_product_description_tab', 25 );
+//add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_additional_information_tab' );
+//add_action( 'woocommerce_after_single_product_summary', 'comments_template' );
